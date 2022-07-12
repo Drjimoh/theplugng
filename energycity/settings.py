f2f7f8a25f6da3a88cb0078be5295d6b3bb56164
swaggu <swagglordbm@gmail.com>
2022-07-12 17:31:46 +0000

    first commit3

--- energycity/settings.py
+++ energycity/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jbb7gpt2!biwkp$r&)ua%h3o95*pgob)k9aq@0p7g(_vt^xjw9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost.com', 'api.localhost.com', '127.0.0.1', '*']
 
